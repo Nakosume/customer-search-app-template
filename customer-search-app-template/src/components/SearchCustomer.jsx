c1767e87173541714200aa6b45452f0f111705f0
Sebastian Romero <nakosume@gmail.com>
2024-04-22 19:04:44 -0500

    Done!

--- customer-search-app-template/src/components/SearchCustomer.jsx
+++ customer-search-app-template/src/components/SearchCustomer.jsx
@@ -2,41 +2,6 @@ import { useEffect, useState } from "react";
 import { Table } from "./Table";
 import getCustomers from "../services/getCustomers";
 
-var clientList =[
-    {
-      id: 1,
-      name:'Alberto',
-      age: '21',
-      location: 'Cali',
-      gender: 'Male',
-      income: '$102,000'
-    },
-    {
-      id: 2,
-      name:'Alberta',
-      age: '35',
-      location: 'Cali',
-      gender: 'Female',
-      income: '$150,000'
-    },
-    {
-      id: 3,
-      name:'Armando',
-      age: '42',
-      location: 'Los Angeles',
-      gender: 'Male',
-      income: '$300,000'
-    },
-    {
-      id: 4,
-      name:'Talia',
-      age: '25',
-      location: 'Florida',
-      gender: 'Female',
-      income: '$102,000'
-    }
-  ]
-
 function SearchCustomer() {
     const [item, setItem] = useState ('')
     const [clients,setClients] = useState([])
@@ -56,7 +21,7 @@ function SearchCustomer() {
 
     const filterClients = (clientL,searchIt) => (clientL.filter((client) =>
       client.name.includes(searchIt)||
-      client.age.includes(searchIt)||
+      client.age.toString().includes(searchIt)||
       client.location.includes(searchIt)||
       client.gender.includes(searchIt)||
       client.income.includes(searchIt)
